2951cab3a62b0b44d0c19b882e74f5e1e6a32153
aki1ro <78289336+aki1ro@users.noreply.github.com>
2021-04-12 16:54:00 -0700

    updated!

---
+++ Python Teaching/Modules and Packages/My Package/superheroes/tmnt/donatello.py
@@ -0,0 +1,11 @@
+#!etc/env/python3 
+
+''' Donatello code'''
+
+def HeroD():
+   return "You've chosen the Ninja Turtle Donatello!"
+
+if __name__ == '__main__':
+   print("I'm a module running in the terminal")
+
+
